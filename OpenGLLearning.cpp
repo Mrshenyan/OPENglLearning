9edb308d45444aee7f45ff1ffbcc2200ba240817
13085028720 <2895289240@qq.com>
2020-06-24 16:57:56 +0800

    first triangle

--- OpenGLLearning.cpp
+++ OpenGLLearning.cpp
@@ -13,35 +13,115 @@ void processInput(GLFWwindow* window) {
 		glfwSetWindowShouldClose(window, true);
 	}
 }
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCT_HEIGHT = 600;
+/// <summary>
+/// 顶点着色器 GLSL代码
+/// </summary>
+const char *vertexShaderSource = "#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char * framgmentShaderSource ="#version 460 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
 int main()
 {
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	GLFWwindow* window = glfwCreateWindow(350, 250, "LearnOpenGL", NULL,NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCT_HEIGHT, "LearnOpenGL", NULL,NULL);
 	if (window == NULL) {
 		cout << "failed to create GLFW window" << endl;
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		cout << "failed to initialize GLAD" << endl;
 		return -1;
 	}
-	
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	int success;
+	char infoLog[512];
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		cout << "ERROR: shader::vertex::compilation failed\n" << infoLog << endl;
+	}
+
+	int framgmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(framgmentShader, 1, &framgmentShaderSource, NULL);
+	glCompileShader(framgmentShader);
+	glGetShaderiv(framgmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(framgmentShader, 512, NULL, infoLog);
+		cout << "ERROR: shader::framgment::compilation failed\n" << infoLog << endl;
+	}
+
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, framgmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+
+	///opengl的核心模式要求使用VAO
+	/// <summary>
+	/// 顶点缓冲对象 vertex buffer obj，顶点数组对象 vertex array obj
+	/// </summary>
+	/// <returns></returns>
+	unsigned int VBO,VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 03, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+	glUseProgram(shaderProgram);
+
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	//someOpenGLFunctionThatDrawOurTriangle();
+
 
 	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		processInput(window);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	glDeleteVertexArrays(1,&VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 	glfwTerminate();
 	return 0;
 }
